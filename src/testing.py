6813fa937f88c465dbb94bebadfaa50ce61f9512
jakepatock <jake.r.patock@gmail.com>
2023-12-21 09:38:42 -0600

    added logos

--- src/testing.py
+++ src/testing.py
@@ -1,26 +1,29 @@
 import ast
 import numpy as np
+import torch
 
-with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_accuracy.txt', 'r') as file:
-    content = file.read()
-    test_accruacy = ast.literal_eval(content)
+# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_accuracy.txt', 'r') as file:
+#     content = file.read()
+#     test_accruacy = ast.literal_eval(content)
 
-with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_loss.txt', 'r') as file:
-    content = file.read()
-    test_loss = ast.literal_eval(content)
+# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_loss.txt', 'r') as file:
+#     content = file.read()
+#     test_loss = ast.literal_eval(content)
 
-with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_accuracy.txt', 'r') as file:
-    content = file.read()
-    train_accruacy = ast.literal_eval(content)
+# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_accuracy.txt', 'r') as file:
+#     content = file.read()
+#     train_accruacy = ast.literal_eval(content)
 
-with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_loss.txt', 'r') as file:
-    content = file.read()
-    train_loss = ast.literal_eval(content)
+# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_loss.txt', 'r') as file:
+#     content = file.read()
+#     train_loss = ast.literal_eval(content)
 
-with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\f1_score.txt', 'r') as file:
-    content = file.read()
-    f1_score = ast.literal_eval(content)
+# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\f1_score.txt', 'r') as file:
+#     content = file.read()
+#     f1_score = ast.literal_eval(content)
 
-arr = np.array(f1_score)
-idx = np.argmax(arr)
-print(train_accruacy[idx])
+# arr = np.array(f1_score)
+# idx = np.argmax(arr)
+#print(train_accruacy[idx])
+
+print(torch.__version__)
