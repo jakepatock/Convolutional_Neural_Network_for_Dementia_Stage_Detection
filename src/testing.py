8ffb60f38a229ab72ddbf7c80b209dc39d7a5831
jakepatock <jake.r.patock@gmail.com>
2023-12-21 14:46:13 -0600

    readme changes

--- src/testing.py
+++ src/testing.py
@@ -7,28 +7,30 @@ import numpy as np
 import torchmetrics
 import PIL 
 import ast
-# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_accuracy.txt', 'r') as file:
-#     content = file.read()
-#     test_accruacy = ast.literal_eval(content)
+with open(r'model_results\16_batch_data\test_accuracy.txt', 'r') as file:
+    content = file.read()
+    test_accruacy = ast.literal_eval(content)
 
-# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\test_loss.txt', 'r') as file:
-#     content = file.read()
-#     test_loss = ast.literal_eval(content)
+with open(r'model_results\16_batch_data\test_loss.txt', 'r') as file:
+    content = file.read()
+    test_loss = ast.literal_eval(content)
 
-# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_accuracy.txt', 'r') as file:
-#     content = file.read()
-#     train_accruacy = ast.literal_eval(content)
+with open(r'model_results\16_batch_data\training_accuracy.txt', 'r') as file:
+    content = file.read()
+    train_accruacy = ast.literal_eval(content)
 
-# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\training_loss.txt', 'r') as file:
-#     content = file.read()
-#     train_loss = ast.literal_eval(content)
+with open(r'model_results\16_batch_data\training_loss.txt', 'r') as file:
+    content = file.read()
+    train_loss = ast.literal_eval(content)
 
-# with open(r'C:\Users\1234z\Desktop\Jakes Stuff\Data\16_batch_data\f1_score.txt', 'r') as file:
-#     content = file.read()
-#     f1_score = ast.literal_eval(content)
+with open(r'model_results\16_batch_data\f1_score.txt', 'r') as file:
+    content = file.read()
+    f1_score = ast.literal_eval(content)
 
-# arr = np.array(f1_score)
-# idx = np.argmax(arr)
-#print(train_accruacy[idx])
+arr = np.array(test_accruacy)
+idx = np.max(arr)
+print(idx)
 
-print(ast.__version__)
+#print(train_loss[idx])
+
+
